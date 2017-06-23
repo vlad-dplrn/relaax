33b4c2d6fde618f335134a6b97f0555f232774ca
dennis <nemiroff.den@gmail.com>
2017-06-23 19:21:59 +0300

    upd: reformat order of layers wrt lstm

--- relaax/algorithms/da3c/da3c_model.py
+++ relaax/algorithms/da3c/da3c_model.py
@@ -25,21 +25,22 @@ class Network(subgraph.Subgraph):
         if da3c_config.config.use_lstm:
             lstm = layer.LSTM(graph.Expand(dense, 0), size=sizes[-1])
             head = graph.Reshape(lstm, [-1, sizes[-1]])
+        layers = [input, dense]
 
         actor = layer.Actor(head, da3c_config.config.output)
         critic = layer.Dense(head, 1)
 
         self.ph_state = input.ph_state
         if da3c_config.config.use_lstm:
+            layers.append(lstm)
             self.ph_lstm_state = lstm.ph_state
             self.ph_lstm_step = lstm.ph_step
             self.lstm_zero_state = lstm.zero_state
             self.lstm_state = lstm.state
         self.actor = actor
         self.critic = graph.Flatten(critic)
-        layers = [input, dense, actor, critic]
-        if da3c_config.config.use_lstm:
-            layers.append(lstm)
+
+        layers.extend((actor, critic))
         self.weights = layer.Weights(*layers)
 
 
